58aab0ddb18eeb49328570dbb3018e97ceea59c5
Chris Werkheiser <cjwerk@gmail.com>
2020-11-01 14:12:33 -0800

    started local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var apiKey = "60ce7466ebde7b0aecd67dbb4864a061";
 var searchBtn = $(".searchBtn");
 var searchInput = $(".searchInput");
+document.getElementsByClassName(".currentDate").innerHTML = Date();
+
 
 
 var cityNameEl= $(".cityName");
@@ -13,3 +15,10 @@ var humidityEl = $(".humidity");
 var windSpeedEl = $(".windSpeed");
 var uvIndexEl = $(".uvIndex");
 var cardRow = $(".card-row");
+
+if(JSON.parse(localStorage.getItem("searchHistory")) === null) {
+    console.log("searchHistory not found")
+} else{
+    console.log("searchHistory loaded into searchHistoryArr");
+    renderSearchHistory();
+}
